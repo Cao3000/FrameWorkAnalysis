523dd11445d68a4132764024634d1f1a6450a373
Cao3000 <37093502+Cao3000@users.noreply.github.com>
2018-11-28 21:47:04 -0500

    honestly how tf do i do this github stuff
    placeholder stuff for installation page honestly how tf do i do this github stuff

--- client/src/FWInstallation.js
+++ client/src/FWInstallation.js
@@ -1,16 +1,127 @@
 import React, {Component} from 'react';
-import NavBar from "./NavBar";
-
 import styled from 'styled-components';
-
+import NavBar from "./NavBar";
+import TableContents from "./TableContents";
 export default class FWInstallation extends Component {
+    
     render(){
+        
         return(
             <div>
                 <NavBar/>
-                This is how you install frameworks.
+                <div>
+                    <h1 id = "react">
+                        React Installation
+                    </h1>
+                    <h2 id = "requirements">
+                        Requirements
+                    </h2> 
+                    <p>
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                    </p>  
+                    <h2 id = "stuff">
+                        Stuff
+                    </h2>
+                    <p>
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                    </p>  
+                    <h1 id ="backend" >
+                        Backend Installation
+                    </h1>
+                </div>
+                <div>
+                <TableContents>
+                    <a href = "#react">React Installation</a>
+                    <a href = "#requirements">Requirements</a>
+                    <a href = "#stuff">Stuff</a>
+                    <a href = "#backend">Backend Installation</a>
+                </TableContents>
+                </div>
             </div>
+            
         )
     }
-
+    
 }
