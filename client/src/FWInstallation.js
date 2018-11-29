8d3426be8670f34bb6cf86ec314e40bec1618c2e
Cao3000 <37093502+Cao3000@users.noreply.github.com>
2018-11-28 21:32:59 -0500

    for table of contents
    Some placeholder stuff for table of contents on right side

--- client/src/FWInstallation.js
+++ client/src/FWInstallation.js
@@ -1,13 +1,54 @@
 import React, {Component} from 'react';
-import styled from 'styled-components';
-
+import NavBar from "./NavBar";
+import TableContents from "./TableContents";
 export default class FWInstallation extends Component {
+    
     render(){
+        
         return(
             <div>
-                This is how you install frameworks.
+                <NavBar/>
+                <div>
+                    <h1 id = "react">
+                        React Installation
+                    </h1>
+                    <h2 id = "requirements">
+                        Requirements
+                    </h2> 
+                    <p>
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                    </p>  
+                    <h2 id = "stuff">
+                        Stuff
+                    </h2>
+                    <p>
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                        Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet Yeet 
+                    </p>  
+                    <h1 id ="backend" >
+                        Backend Installation
+                    </h1>
+                </div>
+                <div>
+                <TableContents>
+                    <a href = "#react">React Installation</a>
+                    <a href = "#requirements">Requirements</a>
+                    <a href = "#stuff">Stuff</a>
+                    <a href = "#backend">Backend Installation</a>
+                </TableContents>
+                </div>
             </div>
+            
         )
     }
-
+    
 }
